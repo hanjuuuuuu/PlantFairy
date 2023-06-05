b7b45a089932796e530a5e37e0147fbce8a24c5a
hanjuuuuuu <hanjucomss@naver.com>
2023-06-05 14:10:04 +0900

    todo db연결

--- api/index.js
+++ api/index.js
@@ -57,7 +57,7 @@ app.post('/recommend', async (req, res) => {
           return { korName: korName, englishName: englishName, context };
         });
 
-      // MySQL 데이터베이스에 데이터 삽입
+      // MySQL 데이터베이스에 식물 데이터 삽입
       const sqlInsert = 'INSERT IGNORE INTO plant(plant_name, eng_name, plant_charateristic) VALUES (?, ?, ?)';
       plantRecommendations.forEach((recommendation) => {
         db.query(sqlInsert, [recommendation.korName, recommendation.englishName, recommendation.context || ''], (err, result) => {
@@ -66,7 +66,6 @@ app.post('/recommend', async (req, res) => {
           }
         });
       });
-
       res.json({ message: plantRecommendations });
     } else {
       res.json({ message: 'Error fetching plant recommendations' });
@@ -76,6 +75,72 @@ app.post('/recommend', async (req, res) => {
     res.status(500).json({ message: 'Internal server error' });
   }
 });
+
+// 식물 투두리스트 생성
+let todoRecommendations;
+app.post('/rectodo', async (req, res) => {
+  const plant =  req.body;
+  console.log('recplantname',plant.plantname)
+  console.log('recuserplantnum', plant.userplantnum)
+  console.log('recusernum',plant.usernum)
+  try {
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `Please make a to-do list in Korean for 31 days with one thing to do to grow ${plant.plantname}, and the answer format is numbered as 1.2.3 `,
+      max_tokens: 4000,
+      temperature: 0.2,
+    });
+
+    console.log('_____________________________',response.data);
+
+    if (response.data && response.data.choices) {
+      todoRecommendations = response.data.choices[0].text
+        .trim()
+        .split("\n")
+        .filter((recommendation) => recommendation)
+        .map((recommendation) => {
+          const [day, context] = (recommendation || '').trim().split(".");
+          console.log('day', day, 'context', context);
+          return { day: day, context: context };
+        });
+
+      // MySQL 데이터베이스에 식물 투두리스트 데이터 삽입
+      const sqlInsert = 'INSERT INTO todo(user_num, user_plant_num, day, task, complete) VALUES (?, ?, ?, ?, ?)';
+      todoRecommendations.forEach((recommendation) => {
+        db.query(sqlInsert, [plant.usernum, plant.userplantnum, recommendation.day, recommendation.context, 'false'], (err, result) => {
+          if (err) {
+            console.log(err);
+          }
+        });
+      });
+      res.json({ message: todoRecommendations });
+    } else {
+      res.json({ message: 'Error todo recommendations' });
+    }
+  } catch (error) {
+    console.log(error.response);
+  }
+})
+
+//todo 페이지에 출력할 식물 투두리스트 전달
+app.post('/planttodo', async (req, res) => {
+  let plantname = req.body.plantname;
+  let userplantnum = 93;
+  let usernum = req.body.usernum;
+  let tododay = req.body.day;
+  console.log('todo plantname', plantname, 'todo userplantnum', userplantnum, 'todo day', tododay);
+
+  const sqlplanttodo = `SELECT todo_num AS "key", task, complete, day FROM todo WHERE user_plant_num = '${userplantnum}'`;
+  db.query(sqlplanttodo, (err, data) => {
+    if(!err){
+      console.log('planttodo', data)
+      res.send(data);
+    }
+    else {
+      console.log(err);
+    }
+  })
+})
 
 // 메인 페이지에 출력할 메인 식물 정보 전달
 app.post('/plantpicture', async (req, res) => {   
@@ -92,6 +157,7 @@ app.post('/plantpicture', async (req, res) => {
     }
   })
 })
+
 
 // 메인 페이지에서 메인으로 지정할 식물 고를 수 있게 사용자의 등록된 전체 식물 이름 전달 
 app.post('/plantall', async (req, res) => {   
@@ -132,11 +198,10 @@ app.post('/plantenroll', async(req, res) => {
   let plantmain = req.body.plantmain;
   let plantpicture = req.body.plantpicture;
   let plantcharacteristic = req.body.plantcharacteristic;
-  let plantlevel = req.body.plantlevel;
   console.log('enroll',plantname);
   
-  const sqlplantenroll = 'INSERT INTO user_plant (user_num, plant_name, plant_main, plant_picture, plant_characteristic, plant_level) values(?, ?, ?, ?, ?, ?)';
-  db.query(sqlplantenroll, [usernum, plantname, plantmain, plantpicture, plantcharacteristic, plantlevel], (err,  data)=> {
+  const sqlplantenroll = 'INSERT INTO user_plant (user_num, plant_name, plant_main, plant_picture, plant_characteristic) values(?, ?, ?, ?, ?)';
+  db.query(sqlplantenroll, [usernum, plantname, plantmain, plantpicture, plantcharacteristic], (err,  data)=> {
     if(!err){
       res.send(data);
     }
@@ -146,88 +211,121 @@ app.post('/plantenroll', async(req, res) => {
   })
 })
 
+//성격 별 식물 추천
+let plantGames
+app.post('/plantgame', async(req, res) => {
+  const { message } = req.body;
+  console.log(message);
+  try {
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `${message} Please express your disposition in 3 lines in Korean and recommend a suitable plant along with the reason. (without the word ${message})`,
+      max_tokens: 1000,
+      temperature: 0.8,
+    });
+
+    console.log(response.data);
+
+    if (response.data && response.data.choices) {
+      plantGames = response.data.choices[0].text
+        .trim()
+        .split(/\d+\./)
+        .filter((recommendation) => recommendation)
+        .map((recommendation) => {
+          console.log('context', recommendation);
+          return { context: recommendation };
+        });
+        res.json({ message: plantGames });
+    } else {
+      res.json({ message: 'Error plantgame' });
+    }
+  } catch (error) {
+    console.log(error.response);
+  }
+})
+
 // 여기는 돈나감!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-app.post('/', async (req, res) => {
-  //const { message } = req.body;
-  //console.log(message);
-  const images = [];
+// app.post('/', async (req, res) => {
+//   //const { message } = req.body;
+//   //console.log(message);
+//   const images = [];
 
-  if (!plantRecommendations || !plantRecommendations.length) {
-    console.log('plantRecommendations is not defined or is empty');
-    return res.status(400).json({ message: 'plantRecommendations is not defined or is empty' });
-  }
+//   if (!plantRecommendations || !plantRecommendations.length) {
+//     console.log('plantRecommendations is not defined or is empty');
+//     return res.status(400).json({ message: 'plantRecommendations is not defined or is empty' });
+//   }
 
-  for (let i = 0; i < plantRecommendations.length; i++) {
-    const response = await openai.createImage({
-      prompt: `${plantRecommendations[i].name}`,
-      n: 1,
-      size: '256x256',
-    });
-    const image_url = response.data.data[0].url;
-    images.push(image_url);
-  }
+//   for (let i = 0; i < plantRecommendations.length; i++) {
+//     const response = await openai.createImage({
+//       prompt: `${plantRecommendations[i].name}`,
+//       n: 1,
+//       size: '256x256',
+//     });
+//     const image_url = response.data.data[0].url;
+//     images.push(image_url);
+//   }
 
-  if (images.length > 0) {
-    for (let i = 0; i < images.length; i++) {
-      const options = {
-        url: images[i],
-        dest: '../../sources',
-      };
+//   if (images.length > 0) {
+//     for (let i = 0; i < images.length; i++) {
+//       const options = {
+//         url: images[i],
+//         dest: '../../sources',
+//       };
 
-      download
-        .image(options)
-        .then(({ filename }) => {
-          console.log('Saved to', filename); // saved to /path/to/dest/image.jpg
+//       download
+//         .image(options)
+//         .then(({ filename }) => {
+//           console.log('Saved to', filename); // saved to /path/to/dest/image.jpg
 
-          const imagePath = path.join('sources/', path.basename(filename)).replace(/\\/g, '/');
-          // Save the path to the database
-          db.query(`UPDATE plant SET img ='${imagePath}' WHERE plant_name='${plantRecommendations[i].name}'`, (error, results) => {
-            if (error) {
-              console.log(error);
-              res.status(500).send('Error saving image path to the database');
-            }
-          });
-        })
-        .catch((err) => {
-          console.error(err);
-          res.status(500).send('Error saving image to local file system');
-        });
-    }
+//           const imagePath = path.join('sources/', path.basename(filename)).replace(/\\/g, '/');
+//           // Save the path to the database
+//           db.query(`UPDATE plant SET img ='${imagePath}' WHERE plant_name='${plantRecommendations[i].name}'`, (error, results) => {
+//             if (error) {
+//               console.log(error);
+//               res.status(500).send('Error saving image path to the database');
+//             }
+//           });
+//         })
+//         .catch((err) => {
+//           console.error(err);
+//           res.status(500).send('Error saving image to local file system');
+//         });
+//     }
 
-    res.json({
-      message: 'Image creation complete',
-      images: images,
-    });
-  } else {
-    res.status(500).send('Error creating images');
-  }
-});
+//     res.json({
+//       message: 'Image creation complete',
+//       images: images,
+//     });
+//   } else {
+//     res.status(500).send('Error creating images');
+//   }
+// });
 
-// 이미지 받아오는 기능
+// // 이미지 받아오는 기능
 
-app.get('/images/:engName', (req, res) => {
-  const engName = req.params.engName.replace(/\n/g, '');
-  console.log(`engName = ${engName}`);
-  db.query(`SELECT plant_picture FROM plant WHERE eng_name = '${engName}'`, (err, result) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send('Server Error');
-    }
-    if (result.length === 0) {
-      return res.status(404).send('Plant not found');
-    }
-    const imgPath = result[0].img;
-    console.log(`imgPath = ${imgPath}`);
-    const imgExists = fs.existsSync(imgPath);
-    if (!imgExists) {
-      return res.status(404).send('Image not found');
-    }
-    const imgFile = fs.readFileSync(imgPath);
-    res.writeHead(200, { 'Content-Type': 'image/png' }); 
-    res.end(Buffer.from(imgFile).toString('base64'));
-  });
-});
+// app.get('/images/:engName', (req, res) => {
+//   const engName = req.params.engName.replace(/\n/g, '');
+//   console.log(`engName = ${engName}`);
+//   db.query(`SELECT plant_picture FROM plant WHERE eng_name = '${engName}'`, (err, result) => {
+//     if (err) {
+//       console.error(err);
+//       return res.status(500).send('Server Error');
+//     }
+//     if (result.length === 0) {
+//       return res.status(404).send('Plant not found');
+//     }
+//     const imgPath = result[0].img;
+//     console.log(`imgPath = ${imgPath}`);
+//     const imgExists = fs.existsSync(imgPath);
+//     if (!imgExists) {
+//       return res.status(404).send('Image not found');
+//     }
+//     const imgFile = fs.readFileSync(imgPath);
+//     res.writeHead(200, { 'Content-Type': 'image/png' }); 
+//     res.end(Buffer.from(imgFile).toString('base64'));
+//   });
+// });
 
 
 app.listen(8800, () => {
